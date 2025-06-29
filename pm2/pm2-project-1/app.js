09955fb3b3f36b32576bd8c745b5c55062b17efc
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-06-29 16:02:23 +0530

    pm2 config

---
+++ pm2/pm2-project-1/app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import morgan from 'morgan';
+
+const server = express();
+const PORT = 5110;
+
+server.use(morgan('dev'));
+
+server.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: `Health check successful, worker: ${process.pid}`
+  })
+})
+
+server.get("/user-details", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "user fetched successfully",
+    id: 101,
+    name: "Swapnil Dutta",
+    email: "swapnildutta2002@gmail.com",
+    mobile: "7999600540"
+  })
+})
+
+server.listen(PORT, () => {
+  console.log(`Server running on PORT: ${PORT}`);
+
+})
